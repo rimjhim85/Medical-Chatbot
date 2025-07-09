c4b783e45b458b773e31f3e174f2a2e4f7d5518f
rimjhim85 <rimjhimchowdhury995@gmail.com>
2025-07-09 18:03:55 +0530

    Add full project files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Rimjhim Chowdhury',
+    author_email= 'rimjhimchowdhury995@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
