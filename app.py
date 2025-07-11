a1c6ec519458327a5ba470caa46f2fa859afee56
rimjhim85 <rimjhimchowdhury995@gmail.com>
2025-07-11 15:20:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -124,4 +124,4 @@ def chat():
 
 if __name__ == '__main__':
     print("Starting Flask app...")
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=False)
